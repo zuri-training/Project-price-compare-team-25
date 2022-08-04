5aebadf160f3ba67cddab0bb3448663467808ee9
VicvekSr <Olamideoso65@gmail.com>
2022-08-04 12:35:12 +0000

    password reset views

--- pricingHub/accounts/views.py
+++ pricingHub/accounts/views.py
@@ -63,32 +63,34 @@ def logout(request):
     return redirect('login')
 
 def password_reset_request(request):
-	if request.method == "POST":
-		password_reset_form = PasswordResetForm(request.POST)
-		if password_reset_form.is_valid():
-			data = password_reset_form.cleaned_data['email']
-			associated_users = User.objects.filter(Q(email=data))
-			if associated_users.exists():
-				for user in associated_users:
-					subject = "Password Reset Requested"
-					email_template_name = "main/password/password_reset_email.txt"
-					c = {
-					"email":user.email,
-					'domain':'',
-					'site_name': 'pricinghub',
-					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
-					'token': default_token_generator.make_token(user),
-					'protocol': 'https',
-					}
-					email = render_to_string(email_template_name, c)
-					try:
-						send_mail(subject, email, 'AWS_verified_email_address', [user.email], fail_silently=False)
-					except BadHeaderError:
-
-						return HttpResponse('Invalid header found.')
-						
-					messages.success(request, 'A message with reset password instructions has been sent to your inbox.')
-					return redirect ("main:homepage")
-			messages.error(request, 'An invalid email has been entered.')
-	password_reset_form = PasswordResetForm()
-	return render(request=request, template_name="main/password/password_reset.html", context={"password_reset_form":password_reset_form})
+    if request.method == "POST":
+        domain = request.headers['Host']
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            # You can use more than one way like this for resetting the password.
+            # ...filter(Q(email=data) | Q(username=data))
+            # but with this you may need to change the password_reset form as well.
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "admin/accounts/password/password_reset_email.txt"
+                    c = {
+                        "email": user.email,
+                        'domain': domain,
+                        'site_name': 'Interface',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+                    email = render_to_string(email_template_name, c)
+                    try:
+                        send_mail(subject, email, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return redirect("/core/password_reset/done/")
+    password_reset_form = PasswordResetForm()
+    return render(request=request, template_name="admin/accounts/password/password_reset.html",
+                  context={"password_reset_form": password_reset_form})
