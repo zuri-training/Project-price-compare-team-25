d7022a9a62c5fefce02f0f2c3ce3479578e6d982
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-08-07 02:15:04 +0100

    settings config to views

--- pricingHub/accounts/views.py
+++ pricingHub/accounts/views.py
@@ -9,6 +9,7 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
+from django.conf import settings
 
 
 def home(request):
