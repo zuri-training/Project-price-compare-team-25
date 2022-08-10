d0bfbcfa37bee9134e710eef552da65ad935b6a6
Est200 <olajideesther2019@gmail.com>
2022-08-10 10:29:33 +0100

    user review

--- pricingHub/reviews/apps.py
+++ pricingHub/reviews/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommentsConfig(AppConfig):
+class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'comments'
+    name = 'reviews'
