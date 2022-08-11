c49092eb98b5757b17a721a3607d000cabb0019b
Jomoartz <johnjohananmogbolu@gmail.com>
2022-08-11 04:27:22 +0100

    more changes

--- pricingHub/products/urls.py
+++ pricingHub/products/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.category, name='category'),
-    path('product', views.product, name='product'),
-    path('comment', views.comment, name='comment'),
-    path('postcomment', views.createcomment, name='postcomment'),
-    path('productdetail', views.productdetail, name='productdetail')
+    path('category/', views.category, name='category/'),
+    path('product/', views.product, name='product/'),
+    path('comment/', views.comment, name='comment/'),
 ]
