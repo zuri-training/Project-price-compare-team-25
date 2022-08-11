ebf1db93db306a89493e83191c06dd77de90846e
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-08-11 06:13:42 +0100

    product url modified

--- pricingHub/products/urls.py
+++ pricingHub/products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('category/', views.category, name='category/'),
-    path('product/', views.product, name='product/'),
-    path('comment/', views.comment, name='comment/'),
+    path('category', views.category, name='category'),
+    path('product', views.product, name='product'),
+    path('comment', views.comment, name='comment'),
 ]
