1400f8cfea09d8358e9d69621c20c039d5f5fa2b
Unique-Red <vnoah410@gmail.com>
2022-08-12 02:08:30 +0100

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python pricingHub/manage.py runserver 0.0.0.0:5000
+web: python pricingHub/manage.py runserver
--- pricingHub/pricingHub/settings.py
+++ pricingHub/pricingHub/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
-from decouple import config
+# from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
