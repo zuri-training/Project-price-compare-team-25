f02d91c04f8a982878313e8237dc2be8614d2bef
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-08-13 04:47:45 +0100

    linked review page,resolved pending error

--- pricingHub/reviews/urls.py
+++ pricingHub/reviews/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-app_name = "reviews"
+
 
 urlpatterns = [
-    path('review', views.review, name='review')
+    path('review/', views.review, name='review')
 ]
