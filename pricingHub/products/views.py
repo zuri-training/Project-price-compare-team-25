96ee031cc7ca5e9fbacead277a0eaffb8def804d
Jomoartz <johnjohananmogbolu@gmail.com>
2022-08-13 19:08:41 +0100

    edited data in database

--- pricingHub/products/views.py
+++ pricingHub/products/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 def category(request):
     categories = Category.objects.all()
-    return render(request, 'category.html', {'categories': categories})
+    return render(request, 'product.html', {'categories': categories})
 
 
 def product(request):
@@ -12,7 +12,7 @@ def product(request):
     return render(request, 'product.html', {'products': products})
 
 def product_details(request):
-    return render (request, 'product_detail_page/product_detail.html')
+    return render(request, 'product_detail_page/product_detail.html')
 
 def comment(request):
     comments = Comment.objects.all()
