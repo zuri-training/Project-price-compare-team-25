e36677b50541468dc37e0de84ab6c449321ea744
Funsho Ayobanjo <funshoayobanjo@gmail.com>
2022-08-15 13:40:53 +0100

    products thumbnail

--- pricingHub/products/views.py
+++ pricingHub/products/views.py
@@ -38,20 +38,3 @@ class AddCommentView(CreateView):
     #fields = '__all__'
 
     success_url = reverse_lazy('comment')
-def post_detailview(request, id):
-   
-  if request.method == 'POST':
-    cf = CreateComments(request.POST or None)
-    if cf.is_valid():
-      content = request.POST.get('content')
-      comment = Comment.objects.create(post = post, user = request.user, content = content)
-      comment.save()
-      return redirect(post.get_absolute_url())
-    else:
-      cf = CreateComments()
-       
-    context ={
-      'comment_form':cf,
-      }
-    return render(request, 'comment.html', context)
-
